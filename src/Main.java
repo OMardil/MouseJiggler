0db86daed6b0c0d32d1b5f149bd1a37925c18b88
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2018-09-21 13:08:42 -0500

    Feat/improved input (#1)
    * Added Seconds Spinner
    
    Implemented a seconds spinner. limited to 60 seconds because we'll be adding minute and hour spinners that autoincrement based on seconds incrementing next.
    
    * Hours, Minutes input added
    
    The minutes and hours spinners have been added, and auto-increment one-another appropriately. Now comes the fun part: ui design
    
    * UI Redesign
    
    Some updates to the UI portion of the application. Next up: Implement decrements in a simililar fashion to the increments.
    
    * Decrement Update
    
    The time "dials" now have behavior to handle both decrements and increments to the respective controls.

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,19 @@ import java.awt.PointerInfo;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 
 
@@ -29,26 +34,57 @@ import javafx.scene.layout.VBox;
  */
 public class Main extends Application
 {
-    int menuSize = 375;
-    int menuSizeTwo = 60;
-    TextField howlong = new TextField();//how long are we jiggling the mouse for?
+    int menuSize = 425;
+    int menuSizeTwo = 135;
+    int minimumSeconds = 0;
+    int maximumSeconds = 60;
+    int minimumMinutes = 0;
+    int maximumMinutes = 60;
+    int minimumHours = 0;
+    int maximumHours = 24;
+    Spinner<Integer> seconds = new Spinner<Integer>(minimumSeconds,maximumSeconds,0,1);
+    Spinner<Integer> minutes = new Spinner<Integer>(minimumMinutes,maximumMinutes,0,1);
+    Spinner<Integer> hours = new Spinner<Integer>(minimumHours,maximumHours,0,1);
     int delayamount = 1000;//how many ms between iterations?
     @Override
     public void start(Stage primaryStage)
     {
-        VBox holder = new VBox();
-        HBox inVBox = new HBox();
-        Text message = new Text("You're inputting how many seconds to jiggle");
+    	setupSpinners();
+        HBox holder = new HBox();
+        HBox labeledSpinners = new HBox();
+        VBox spinners = new VBox();
+        VBox labels = new VBox();
         Button btJiggle = new Button("Jiggle the mouse pls");//button to jiggle the mouse
         btJiggle.setOnAction(new jigglehandler());//wire the button to jiggle the mouse
-        inVBox.getChildren().add(howlong);
-        inVBox.getChildren().add(btJiggle);//put the button and text field next to each other
-        holder.getChildren().add(message);
-        holder.getChildren().add(inVBox);//then put the text and the group from earlier in the window
+        btJiggle.setAlignment(Pos.CENTER);
+        spinners.getChildren().add(seconds);
+        spinners.getChildren().add(minutes);
+        spinners.getChildren().add(hours);
+        labels.getChildren().add(new Text("Seconds:"));
+        labels.getChildren().add(new Text("Minutes:"));
+        labels.getChildren().add(new Text("Hours: "));
+        labeledSpinners.getChildren().add(labels);
+        labeledSpinners.getChildren().add(spinners);
+        holder.getChildren().add(labeledSpinners);
+        holder.getChildren().add(btJiggle);
+        labels.setSpacing(10);
+        labeledSpinners.setSpacing(5);
+        holder.setSpacing(10);
         Scene primscene = new Scene(holder,menuSize,menuSizeTwo);
         primaryStage.setScene(primscene);//make the scene with all the stuff in it and set it to the main window
         primaryStage.show();//show the main window
     }
+	private void setupSpinners() {
+    	seconds.setEditable(false);
+    	minutes.setEditable(false);
+    	hours.setEditable(false);
+    	seconds.getValueFactory().setWrapAround(true);
+    	minutes.getValueFactory().setWrapAround(true);
+    	//hours doesn't wraparound because we don't want to set hours to 24 by lowering minutes without there being an hour in there
+    	seconds.valueProperty().addListener(new secondsIncrementHandler());
+    	minutes.valueProperty().addListener(new minutesChangeHandler());
+    	hours.valueProperty().addListener(new hoursChangeHandler());
+	}
     class jigglehandler implements EventHandler<ActionEvent>
     {
 
@@ -62,7 +98,7 @@ public class Main extends Application
                 int x = (int) b.getX();
                 int y = (int) b.getY();
                 Robot bot = new Robot();
-                howlongnum = Integer.parseInt(howlong.getText());
+                howlongnum = parseInput();
                 bot.setAutoWaitForIdle(true);
                 for(int i = 0; i<howlongnum;i++)
                 {
@@ -82,17 +118,7 @@ public class Main extends Application
                     }//Wait a second, then if it's an even cycle of movement, move it to the right, otherwise, move it to the left
                 }
             }
-            catch(NumberFormatException e)
-            {
-                Stage ErrorStage = new Stage();
-                HBox inside = new HBox();
-                Scene ErrorScene = new Scene(inside,menuSize,menuSizeTwo);
-                Text error = new Text("There was a goof in how long you asked for, try again?");
-                inside.getChildren().add(error);
-                ErrorStage.setScene(ErrorScene);
-                ErrorStage.show();
-                //if they didn't type a number, tell them to type a number
-            } catch (AWTException ex) {
+            catch (AWTException ex) {
                 Stage ErrorStage = new Stage();
                 HBox inside = new HBox();
                 Scene ErrorScene = new Scene(inside,menuSize,menuSizeTwo);
@@ -102,37 +128,74 @@ public class Main extends Application
                 ErrorStage.show();
             }
         }
+
+		private Integer parseInput() {
+			int numberOfSeconds = 0;
+			numberOfSeconds += seconds.getValue();
+			numberOfSeconds += minutes.getValue()*60;
+			numberOfSeconds += hours.getValue()*3600;
+			return numberOfSeconds;
+		}
     
     }
-    public void typeThings(String phrase) 
+    class secondsIncrementHandler implements ChangeListener<Integer>
     {
-        try
-        {
-            Robot robot = new Robot();    
-            for (int i = 0; i < phrase.length(); i++) {
-                char c = phrase.charAt(i);
-                if (Character.isUpperCase(c)) 
-                {
-                    robot.keyPress(KeyEvent.VK_SHIFT);
-                }
-                robot.keyPress(Character.toUpperCase(c));
-                robot.keyRelease(Character.toUpperCase(c));
-                if (Character.isUpperCase(c)) 
-                {
-                    robot.keyRelease(KeyEvent.VK_SHIFT);
-                }
-            }
-        }
-        catch (AWTException ex) 
-        {
-            Stage ErrorStage = new Stage();
-            HBox inside = new HBox();
-            Scene ErrorScene = new Scene(inside,menuSize,menuSizeTwo);
-            Text error = new Text("The Bot Is A N G E R");
-            inside.getChildren().add(error);
-            ErrorStage.setScene(ErrorScene);
-            ErrorStage.show();
-        }
+
+		@Override
+		public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+			if(oldValue == maximumSeconds-1 && newValue == maximumSeconds)
+			{
+				minutes.increment(1);
+				//if they max out seconds, increment minutes
+				//originally we were resetting the seconds manually here
+				//but setting the wraparound property to true does that for us
+			}
+			if(oldValue == minimumSeconds+1 && newValue == minimumSeconds)
+			{
+				minutes.decrement(1);
+			}
+			//essentially, if they max out seconds, increase the minutes, but if they zero out seconds, decrement minute
+		}
+    	
+    }
+    class minutesChangeHandler implements ChangeListener<Integer>
+    {
+
+		@Override
+		public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+			if(oldValue == maximumMinutes-1 && newValue == maximumMinutes)
+			{
+				hours.increment(1);
+				//originally we were setting the values manually here
+				//but setting the wraparound property to true does that for us
+			}
+			if(oldValue == minimumMinutes+1 && newValue == minimumMinutes)
+			{
+				hours.decrement(1);
+			}
+			//essentially, if they max out minutes, increase the hours, but if they zero out minutes, decrement hour
+			if(oldValue == minimumMinutes && newValue == maximumMinutes)
+			{
+				minutes.getValueFactory().setValue(0);
+				//we deal with minute underflow manually though because wraparound is needed in the minutes spinner
+			}
+		}
+    	
+    }
+    class hoursChangeHandler implements ChangeListener<Integer>
+    {
+
+		@Override
+		public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+			if(oldValue == maximumHours-1 && newValue == maximumHours)
+			{
+				seconds.getValueFactory().setValue(minimumSeconds);
+				minutes.getValueFactory().setValue(minimumMinutes);
+				hours.getValueFactory().setValue(minimumHours);
+				//if they max out hours, it resets the whole schpiel
+			}
+		}
+    	
     }
     public static void main(String[] args) {
         launch(args);
